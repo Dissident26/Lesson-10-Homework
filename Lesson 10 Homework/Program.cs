cc7bdf219d22bf58853a4cbc99c906004ee2fff7
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-03-28 14:55:17 +0300

    Add project files.

---
+++ Lesson 10 Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyLib;
+
+MyList myList = new();
+myList.Add(new MyItem(1, "first"));
+myList.Add(new MyItem(1, "second"));
+myList.Add(new MyItem(1, "third"));
+myList.Add(new MyItem(1, "fourth"));
+myList.Add(new MyItem(1, "fifth"));
+myList.Add(new MyItem(1, "sixth"));
+foreach (var item in myList)
+{
+    Console.WriteLine(item);
+}
---
+++ MyLib/MyLib.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+
+namespace MyLib
+{
+    public class MyList : IList<MyItem>
+    {
+        private readonly List<MyItem> Items = new();
+        public MyItem this[int index] { get => Items[index]; set => Items[index] = value; }
+
+        public int Count => Items.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(MyItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+
+        public bool Contains(MyItem item)
+        {
+            return Items.Contains(item);
+        }
+
+        public void CopyTo(MyItem[] array, int arrayIndex)
+        {
+            Items.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<MyItem> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        public int IndexOf(MyItem item)
+        {
+            return Items.IndexOf(item);
+        }
+
+        public void Insert(int index, MyItem item)
+        {
+            Items.Insert(index, item);  
+        }
+
+        public bool Remove(MyItem item)
+        {
+            return Items.Remove(item); 
+        }
+
+        public void RemoveAt(int index)
+        {
+            Items.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class MyItem
+    {
+        public int Id;
+        public string Name;
+
+        public MyItem(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+            public override string ToString()
+        {
+            return $"MyItem: id = {Id}, name = {Name}";
+        }
+    }
+}
